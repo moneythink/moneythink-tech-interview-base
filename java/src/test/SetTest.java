4f4bbc04fd707c93f4f3ff687ac4edc8c1ccae21
Colin Young <me@colinyoung.com>
2016-01-25 08:58:13 -0600

    Add test dir

---
+++ java/src/test/SetTest.java
@@ -0,0 +1,12 @@
+package test;
+import myset.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SetTest {
+	@Test
+	public void testAssertTrue() {
+		Assert.assertEquals(false, true);
+	}
+}
