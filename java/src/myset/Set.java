f7a9937ea13eb88ffe25c517518d3e76ff994248
Colin Young <me@colinyoung.com>
2016-01-25 11:15:30 -0600

    Commit Brandon's interview

--- java/src/myset/Set.java
+++ java/src/myset/Set.java
@@ -6,17 +6,53 @@ public class Set<V> {
 
 	private Item<String,V>[] table;
 
+	private int size = 0;
+
 	public Set() {
 		this(defaultCapacity);
 	}
 
 	public Set(int size) {
+		table = new Item[size];
 	}
 
 	public Object get(String key) {
+		for (int i = 0; i < size; i++){
+			if(table[i].getKey().equals(key)){
+				return table[i].getValue();
+			}
+		}
+
 		return null;
 	}
 
+	private int indexOf(String key) {
+		int index = -1;
+
+		for (int i = 0; i < size; i++){
+			if(table[i].getKey().equals(key)){
+				index = i;
+				break;
+			}
+		}
+
+		return index;
+	}
+
 	public void put(String key, Object value) {
+		int index = this.indexOf(key);
+
+
+		if (index == - 1){
+			table[size] = new Item(key, value);
+			size++;
+		}
+		else{
+			table[index] = new Item(key, value);
+		}
+	}
+
+	public int getSize () {
+		return size;
 	}
 }
--- java/src/test/SetTest.java
+++ java/src/test/SetTest.java
@@ -2,17 +2,51 @@ package test;
 import myset.*;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SetTest {
-	@Test
-	public void testAssertTrue() {
-		Assert.assertEquals(false, true);
-	}
+  public Set emptySet;
 
-	@Test
-	public void testEmptyWithInitalized (){
-		Set emptySet = new Set<String, V> ();
-		Assert.assertEquals(0, emptySet.size);
-	}
+  @Before
+  public void setUp(){
+    emptySet = new Set();
+  }
+
+  @Test
+  public void testEmptyWithInitalized (){
+    Assert.assertEquals(0, emptySet.getSize());
+  }
+
+  @Test
+  public void testCanPutItemIntoSet () {
+    emptySet.put("someKey", new Integer(42));
+
+    Integer i = (Integer)emptySet.get("someKey");
+
+    Assert.assertEquals(i.intValue(), 42);
+
+    Assert.assertEquals(emptySet.getSize(), 1);
+  }
+
+  @Test
+  public void testCanGetItemFromSet () {
+    emptySet.put("getKey", new Integer(50));
+
+    Integer i = (Integer)emptySet.get("getKey");
+
+    Assert.assertEquals(50, i.intValue());
+  }
+
+  @Test
+  public void testKeysAreUnique () {
+    emptySet.put("someKey", new Integer(42));
+    emptySet.put("someKey", new Integer(50));
+
+    Assert.assertEquals(1, emptySet.getSize());
+
+    Integer i = (Integer) emptySet.get("someKey");
+
+    Assert.assertEquals(50, i.intValue());
+  }
 }
